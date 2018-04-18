4c6f7b9b3676685ee01e9f7239797267ba4f627b
623488217@qq.com <WENta1988723>
2018-04-18 09:34:46 +0800

    k9

---
+++ src/studyjava/CollectionClassTests/MapTests.java
@@ -0,0 +1,17 @@
+package studyjava.CollectionClassTests;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+/*
+实现类：HashMap、Hashtable、LinkedHashMap和TreeMap
+*/
+public class MapTests {
+    @BeforeEach
+    void setUp() throws Exception {
+    }
+
+    @AfterEach
+    void tearDown() throws Exception {
+    }
+}
