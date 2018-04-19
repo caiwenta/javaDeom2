30ded8ab3b5af2e93654ffd23480f72c8bb5b6bc
623488217@qq.com <WENta1988723>
2018-04-19 09:10:58 +0800

    k9

--- src/studyjava/CollectionClassTests/MapTests.java
+++ src/studyjava/CollectionClassTests/MapTests.java
@@ -1,8 +1,13 @@
 package studyjava.CollectionClassTests;
 
+import com.sun.org.apache.xml.internal.security.Init;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /*
 实现类：HashMap、Hashtable、LinkedHashMap和TreeMap
@@ -17,11 +22,54 @@ public class MapTests {
     }
 
     /*
+    Map中我们通过对象来对对象进行索引，用来索引的对象叫做key，其对应的对象叫做value
+    */
+    @Test
+    public void MapTest() {
+        Map map = new HashMap();
+        map.put("A", "AAA");
+        map.put(1, 11);
+
+
+        //遍历
+        Iterator iterator = map.keySet().iterator();
+        while (iterator.hasNext()) {
+            Object key = iterator.next();
+            if (key instanceof String) {
+                assertEquals(key, "A");
+            }
+            if (key instanceof Integer) {
+                assertEquals(key, 1);
+            }
+        }
+
+        assertEquals(map.get(1), 11);
+        assertEquals(map.get("A"), "AAA");
+
+        //泛型
+        Map<String, String> map1 = new HashMap<String, String>();
+        map1.put("aa", "11");
+
+        //遍历
+        Collection collection=map1.values();
+        Iterator iterator2 = collection.iterator();
+
+        Map<String, String> map3 = new LinkedHashMap<String, String>();
+        map3.put("justin", "justin的讯息");
+        map3.put("momor", "momor的讯息");
+        map3.put("caterpillar", "caterpillar的讯息");
+        for(String value : map3.values()) {
+            System.out.println(value);
+        }
+    }
+
+    /*
     HashMap是最常用的Map，它根据键的HashCode值存储数据，根据键可以直接获取它的值，具有很快的访问速度，遍历时，取得数据的顺序是完全随机的。
     因为键对象不可以重复，所以HashMap最多只允许一条记录的键为Null，允许多条记录的值为Null，是非同步的
     */
     @Test
     public void HashMapTest() {
+
 
     }
 
