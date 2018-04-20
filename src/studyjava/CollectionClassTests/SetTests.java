761d3ec5cfb50f22a56098fcb170c49ce7d4eb0a
623488217@qq.com <WENta1988723>
2018-04-20 08:42:29 +0800

    学习了Set基本方法的使用

---
+++ src/studyjava/CollectionClassTests/SetTests.java
@@ -0,0 +1,22 @@
+package studyjava.CollectionClassTests;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class SetTests {
+    @BeforeEach
+    void setUp() throws Exception {
+    }
+
+    @AfterEach
+    void tearDown() throws Exception {
+    }
+
+
+    @Test
+    public void SetTest() {
+
+    }
+
+}
