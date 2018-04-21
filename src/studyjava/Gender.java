5244db3c449fe8e7e308031889f13986ec7c531b
623488217@qq.com <WENta1988723>
2018-04-21 09:25:08 +0800

    学习了字符串使用

--- src/studyjava/Gender.java
+++ src/studyjava/Gender.java
@@ -1,13 +1,5 @@
 package studyjava;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-
-import java.io.*;
-import java.util.ArrayList;
-import java.util.List;
-
 public enum Gender {
     MALE,FEMALE;
 
