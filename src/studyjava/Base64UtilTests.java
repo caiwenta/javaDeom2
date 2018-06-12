46190638737305eaadc2b24e4e2bcb15685a5ed3
623488217@qq.com <WENta1988723>
2018-06-12 20:33:03 +0800

    学习了sql server jdbc 数据库连接两种方式使用，

---
+++ src/studyjava/Base64UtilTests.java
@@ -0,0 +1,15 @@
+package studyjava;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+public class Base64UtilTests {
+
+    @BeforeEach
+    void setUp() throws Exception {
+    }
+
+    @AfterEach
+    void tearDown() throws Exception {
+    }
+}
